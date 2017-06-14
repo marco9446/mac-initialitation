f9b08920b752297e570933b61e85b12b7c4577ed
marco ravazzini <marcoravazzini@marcos-MacBook-Pro.local>
2017-06-15 00:47:11 +0200

    Update config files Thu Jun 15 00:47:11 CEST 2017

--- updater.sh
+++ updater.sh
@@ -1,6 +1,6 @@
 cp ~/.zshrc ./program_config/zsh/zsh_config.orig
 cp ~/.vimrc ./program_config/vim/vim_config
-
+cp ~/Library/Application\ Support/Kodi/userdata/favourites.xml ./program_config/kodi/favourites.xml
 
 
 
